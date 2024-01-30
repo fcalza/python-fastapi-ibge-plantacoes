3bb35c7d95b223f66fdaa0623e512779d296f6ff
Calza <you@example.com>
2024-01-30 17:07:34 -0300

    possivel modelo de retorno

--- src/service.py
+++ src/service.py
@@ -1,10 +1,12 @@
 import json
 import requests
+from src.schemas import RetornoConsultaPlantacoes
 
 
 def consulta_area_colhida(year: int) -> json:
     area_colhida = f"https://apisidra.ibge.gov.br/values/t/5457/n6/all/v/216/p/{year}/c782/40124?formato=json"
-    return requests.get(area_colhida).json()
+    consulta_plantacoes = RetornoConsultaPlantacoes.model_validate(requests.get(area_colhida).json())
+    return consulta_plantacoes.model_dump()
 
 
 def consulta_quantidade_produzida(year: int) -> json:
